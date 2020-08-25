a833a868dc1f7310de2efe97fce71d18abf6f9c1
wudf <wudf@fingard.com>
2020-08-25 20:29:06 +0800

    Feat: 继续添加混合的内容

--- src/page/lessContent/Mixins.tsx
+++ src/page/lessContent/Mixins.tsx
@@ -136,6 +136,167 @@ const value7 = `
   .default {
     .border-radius();
   }`
+  const value9 = `
+  .mixina(@color) {
+    color: @color;
+  }
+  .mixina(@color; @padding: 2px) {
+    border:1px solid @color;
+    padding: @padding;
+  }
+  .mixina(@color; @padding; @margin: 2px) {
+    color: @color;
+    padding: @padding;
+    margin: @margin;
+  }
+  .some {
+    .mixina(#6141ee)
+  }
+  // 编译成： (不懂为什么，为什么第三个的都没有编译)
+  .some {
+    color: #6141ee;
+    border: 1px solid #6141ee;
+    padding: 2px;
+  }`
+  const value10 =`
+  .mixinb(@color: blue; @margin: 10px; @padding: 20px) {
+    color: @color;
+    margin: @margin;
+    padding: @padding;
+    border: 1px solid @color;
+  }
+  .class1 {
+    .mixinb(@margin: 20px; @color: green);
+  }
+  .class2 {
+    .mixinb(yellow; @padding: 20px);
+  }
+  .class3 {
+      .mixinb(pink, 20px, 10px)
+  }`
+  const value11 = `
+  .box-shadow(@x: 0; @y: 0; @blur: 1px; @color: #464646) {
+    -webkit-box-shadow: @arguments;
+       -moz-box-shadow: @arguments;
+            box-shadow: @arguments;
+  }
+  .class4 {
+    border: 1px solid pink;
+    .box-shadow(1px; 1px; 3px);
+  }
+  // 结果是
+  .class4 {
+    border: 1px solid pink;
+    -webkit-box-shadow: 1px 1px 3px #464646;
+       -moz-box-shadow: 1px 1px 3px #464646;
+            box-shadow: 1px 1px 3px #464646;
+  }`
+  const value12 = `
+    .mixin(...) {        // matches 0-N arguments
+    .mixin() {           // matches exactly 0 arguments
+    .mixin(@a: 1) {      // matches 0-1 arguments
+    .mixin(@a: 1; ...) { // matches 0-N arguments
+    .mixin(@a; ...) {    // matches 1-N arguments
+    // 此外   
+    .mixin(@a; @rest...) {
+        // @rest is bound to arguments after @a
+        // @arguments is bound to all arguments
+    }`
+const value13 = `
+    .mixinc(dark; @color) {
+        background: darken(@color, 10%);
+    }
+    .mixinc(light; @color) {
+        background: lighten(@color, 20%);
+    }
+    .mixinc(@_; @color) {
+        border: 1px solid;
+    }
+    .class5 {
+        .mixinc(light; #f72346)
+    }
+    // 解析
+    .class5 {
+        border: 1px solid;
+        background: lighten(#f72346, 20%);
+    }
+      `
+const value14 = `
+.average(@x, @y) {
+    @result: ((@x + @y) / 2);
+  }
+.class6 {
+    // call a mixin and look up its "@result" value
+    padding: .average(16px, 50px)[@result];
+    border: 1px solid;
+  }
+  // 解析
+  class6 {
+    border: 1px solid;
+    padding: 33px;
+  }`
+const value15 = `
+.aver(@x, @y) {
+    @result1: ((@x + @y) / 2);
+    @result2: ((@x - @y) / 2);
+    // background-color: pink; 
+  }
+.class7 {
+    // 这里将取.aver的最后一个属性的值，如果最后一个是background-color：pink，则padding：pink，无效。
+    padding: .aver(50px, 16px)[];
+    border: 1px solid;
+  }`
+  const value16 = `
+  .mixind() {
+    @backcolor: pink;
+    @fontcolor: blue;
+  }
+  .caller {
+    .mixind();
+    background: @backcolor;
+    color: @fontcolor;
+  }
+  @backcolor: green;`
+  const value17 =`
+  .unlock(@value) { // outer mixin
+    background-color: purple;
+    .doSomething() { // nested mixin
+      background: @value;
+    }
+  }
+  .class8 {
+    // 这里要先调用.unlock，再调用.doSomething才会显示出background的效果,不然就只是返回.unlock里面的效果。
+    .unlock(pink); // unlock doSomething mixin
+    .doSomething(); //nested mixin was copied here and is usable
+  }`
+  const value18 = `
+  .loop(@counter) when (@counter > 0) {
+    .loop((@counter - 1));    // next iteration
+    width: (10px * @counter); // code for each iteration
+    background: green;
+  }
+  .class9 {
+    .loop(5); // launch the loop
+  }
+  // 解析
+  div {
+    width: 10px;
+    width: 20px;
+    width: 30px;
+    width: 40px;
+    width: 50px;
+  }
+  `
+  const value19 = `
+  .generate-columns(4);
+  .generate-columns(@n, @i: 1) when (@i =< @n) {
+    .div-@{i} {
+      width: (@i * 100% / @n);
+      background: yellowgreen;
+      margin-bottom: 2px;
+    }
+    .generate-columns(@n, (@i + 1));
+  }`
 export default class Easy extends React.Component {
     render() {
         return (
@@ -235,6 +396,120 @@ export default class Easy extends React.Component {
                                 </Col>
                                 <Col span={14}>
                                     <Editor value={value8} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.8 具有多个参数的Mixins</h1>
+                            {/* <p>Mixins也可以接受参数，这些参数是在混合时传递到选择器块的变量。</p> */}
+                            <Row>
+                                <Col span={10}>
+                                    <div className="some">some</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value9} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.9 命名参数</h1>
+                            <p>混合引用可以通过名称而不是位置来提供参数值。可以通过名称来引用任何参数，它们不必采用任何特殊顺序：</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class1">class1</div>
+                                    <div className="class2">class2</div>
+                                    <div className="class3">class3</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value10} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.10 @arguments</h1>
+                            <p>@arguments在mixin内部有特殊含义，它包含调用mixin时传递的所有参数。如果您不想处理单个参数，这将很有用：</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class4">class4</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value11} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.11 高级参数和@rest变量</h1>
+                            <p>...如果您希望mixin接受可变数量的参数，则可以使用。在变量名之后使用此命令会将这些参数分配给变量。</p>
+                            <Editor value={value12}/>
+                            <h1 className="font-bold">2.12 模式匹配</h1>
+                            <p>有时，您可能想根据传递给它的参数来更改混合的行为。让我们从一些基本的东西开始：</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class5">class5</div>
+                                    <ul>
+                                    这是发生了什么：
+                                        <li>第一个mixin定义不匹配，因为它应dark作为第一个参数。</li>
+                                        <li>第二个mixin定义匹配，因为它符合预期light。</li>
+                                        <li>第三个mixin定义匹配，因为它期望任何值。</li>
+                                    </ul>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value13} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.13 使用Mixins作为函数</h1>
+                            <p>从混合调用中选择属性和变量，从Less 3.5开始，您可以使用属性/变量访问器从评估的混合规则中选择一个值。这可以让您使用类似于功能的混合。</p>
+                            <p>如果您有多个匹配的mixin，则会评估并合并所有规则，并返回带有该标识符的最后一个匹配值。这类似于CSS中的层叠，它允许您“覆盖” mixin值。</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class6">class6</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value14} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.14 未命名的查询</h1>
+                            <p>如果您没有在其中指定查找值[@lookup]，而是在mixin或规则集调用之后写入[]，则所有值将级联，并选择最后声明的值。</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class7">class7</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value15} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.15 将mixin和变量解锁到调用方作用域中</h1>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="caller">caller</div>
+                                    <p>像在这个例子中，在调用者.caller范围内的变量是受到保护的，所以即使在外面重新给 @backcolor赋值了，但是.caller内部的@backcolor还是不变。而其外部的@backcolor是不受到保护的，已经被重新赋值。</p>
+                                    <div className="outer">outer</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value16} />
+                                </Col>
+                            </Row>
+                            <p>最后，在mixin中定义的mixin也充当返回值</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class8">class8</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value17} />
+                                </Col>
+                            </Row>
+                            <h1 className="font-bold">2.16 递归混合</h1>
+                            <p>在Less中，mixin可以自称。当与Guard表达式和模式匹配结合使用时，此类递归mixin 可用于创建各种迭代/循环结构。</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="class9">class9</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value18} />
+                                </Col>
+                            </Row>
+                            <p>使用递归循环生成CSS网格类的一般示例：</p>
+                            <Row>
+                                <Col span={10}>
+                                    <div className="div-1">div-1</div>
+                                    <div className="div-2">div-2</div>
+                                    <div className="div-3">div-3</div>
+                                    <div className="div-4">div-4</div>
+                                </Col>
+                                <Col span={14}>
+                                    <Editor value={value19} />
                                 </Col>
                             </Row>
                         </PageHeader>
