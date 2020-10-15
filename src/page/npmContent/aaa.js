2c402e9c3ea95e294b89f019fa1f4b5bc5adb1ea
wudf <wudf@fingard.com>
2020-10-15 20:47:05 +0800

    Feat: 修改图

--- src/page/npmContent/aaa.js
+++ src/page/npmContent/aaa.js
@@ -1,4 +1,4 @@
-const flowChartJD = {
+const flowChartJD1 = {
   code: '0',
   info: '操作成功！',
   value: null,
@@ -30,6 +30,44 @@ const flowChartJD = {
                 type: 'import',
                 text: '导入',
                 url: '/thirdpartypay/salesslip/salesImport.do'
+              }
+            ],
+            children: [
+              {
+                type: 'block',
+                linkText: '合并',
+                text: '日记账零售单1',
+                button: [
+                  {
+                    type: 'view',
+                    text: '查看',
+                    url: 'SalesSlip:list'
+                  },
+                  {
+                    type: 'import',
+                    text: '导入',
+                    url: '/thirdpartypay/salesslip/salesImport.do'
+                  }
+                ],
+                children: [
+                  {
+                    type: 'block',
+                    linkText: '合并',
+                    text: '日记账零售单2',
+                    button: [
+                      {
+                        type: 'view',
+                        text: '查看',
+                        url: 'SalesSlip:list'
+                      },
+                      {
+                        type: 'import',
+                        text: '导入',
+                        url: '/thirdpartypay/salesslip/salesImport.do'
+                      }
+                    ]
+                  }
+                ]
               }
             ]
           }
@@ -127,4 +165,268 @@ const flowChartJD = {
     ]
   }
 }
-export default flowChartJD;
+const flowChartJD = {
+  code: '0',
+  info: '操作成功！',
+  value: null,
+  success: true,
+  data: {
+    type: 'horizontal-cycle', // 固定横向(顶级核对部分)
+    button: [{
+      type: 'check',
+      text: '核对处理',
+      url: ''
+    }],
+    nodeList: [
+      {
+        type: 'block', // 普通“按钮“
+        text: '已合并零售单',
+        button: [{
+          type: 'view',
+          text: '查看',
+          url: 'SalesSlipCheckedData:list',
+          params: { platform: '4' }
+        }],
+        children: [
+          {
+            type: 'block',
+            linkText: '合并',
+            text: '已记账零售单',
+            button: [
+              {
+                type: 'view',
+                text: '查看',
+                url: 'SalesSlip:list'
+              },
+              {
+                type: 'import',
+                text: '导入',
+                url: '/thirdpartypay/salesslip/salesImport.do'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        type: 'block',
+        text: '已合并结算明细',
+        button: [{
+          type: 'view',
+          text: '查看',
+          url: 'SettlementCheckedData:list',
+          params: { platform: '4', tenantid: '10221' }
+        }],
+        children: [
+          {
+            type: 'block',
+            linkText: '合并',
+            text: '已到账结算明细',
+            button: [{
+              type: 'view',
+              text: '查看',
+              url: 'PlatformSettlementDataJD:list',
+              params: { platform: '4', tenantid: '1' }
+            }]
+          },
+          {
+            type: 'vertical-cycle', // 竖向
+            nodeList: [
+              {
+                type: 'block',
+                text: '已汇总结算明细',
+                button: [{
+                  type: 'view',
+                  text: '查看',
+                  url: 'PlatformSettlementDataJD:list',
+                  params: { platform: '4', tenantid: '1' }
+                }]
+              },
+              {
+                type: 'block',
+                text: '钱包账户流水',
+                button: [
+                  {
+                    type: 'view',
+                    text: '查看',
+                    url: 'SalesSlip:list'
+                  },
+                  {
+                    type: 'import',
+                    text: '导入',
+                    url: '/thirdpartypay/salesslip/salesImport.do'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            type: 'block', // 普通“按钮“
+            text: '平台结算明细',
+            linkText: '合并',
+            button: [{
+              type: 'view',
+              text: '查看',
+              url: 'SalesSlipCheckedData:list',
+              params: { platform: '4' }
+            },
+            {
+              type: 'import',
+              text: '导入',
+              url: '/thirdpartypay/salesslip/salesImport.do'
+            }]
+          }
+        ]
+      }
+    ]
+  }
+}
+// const flowChartJD = {
+//   code: '0',
+//   info: '操作成功！',
+//   value: null,
+//   success: true,
+//   rows: [{
+//     id: 1,
+//     name: '已记账零售单',
+//     color: 'bgColor',
+//     view: { url: 'SalesSlip:list' },
+//     import: '/thirdpartypay/salesslip/salesImport.do',
+//     y: 80,
+//     x: 50
+//   }, {
+//     id: 2,
+//     img: 'rightArrow',
+//     y: 80,
+//     x: 150
+//   }, {
+//     id: 3,
+//     name: '已合并零售单',
+//     color: 'bgColor',
+//     view: { url: 'SalesSlipCheckedData:list', param: { platform: '4' } },
+//     import: '',
+//     y: 80,
+//     x: 250
+//   }, {
+//     id: 4,
+//     img: 'cycle',
+//     check: { param: '1' },
+//     y: 80,
+//     x: 350
+//   }, {
+//     id: 5,
+//     name: '已合并结算明细',
+//     color: 'bgColor',
+//     view: { url: 'SettlementCheckedData:list', param: { platform: '4', tenantid: '10221' } },
+//     import: '',
+//     y: 80,
+//     x: 450
+//   }, {
+//     id: 6,
+//     img: 'leftArrow',
+//     y: 80,
+//     x: 560
+//   }, {
+//     id: 7,
+//     name: '已到账结算明细',
+//     color: 'bgColor',
+//     view: { url: 'PlatformSettlementDataJD:list', param: { platform: '4', tenantid: '1' } },
+//     import: '',
+//     y: 80,
+//     x: 650
+//   }, {
+//     id: 6,
+//     img: 'leftArrow',
+//     y: 80,
+//     x: 760
+//   }, {
+//     id: 9,
+//     name: '钱包账户流水',
+//     color: 'bgColor',
+//     view: { url: 'jdAccountFlow:list', param: { platform: '4', tenantid: '1' } },
+//     import: '/thirdpartypay/jdaccountflow/jdAccountFlowImport.do',
+//     y: 80,
+//     x: 850
+//   }, {
+//     id: 10,
+//     img: 'cycle',
+//     y: 80,
+//     x: 970
+//   }, {
+//     id: 11,
+//     name: '已汇总结算明细',
+//     color: 'bgColor',
+//     view: { url: 'PlatformDataCheck:list', param: { platform: '4', tenantid: '1' } },
+//     import: '',
+//     y: 80,
+//     x: 1050
+//   }, {
+//     id: 12,
+//     img: 'leftArrow',
+//     y: 80,
+//     x: 1160
+//   }, {
+//     id: 13,
+//     name: '平台结算明细',
+//     color: 'bgColor',
+//     view: { url: 'PlatformSettlementDataJD:list', param: { platform: '4', tenantid: '1' } },
+//     import: '/thirdpartypay/platformsettlementdatajd/JDSettlementDataImport.do',
+//     y: 80,
+//     x: 1250
+//   }]
+// }
+const flowChartTM = {
+  code: '0',
+  info: '操作成功！',
+  value: null,
+  success: true,
+  rows: [{
+    id: 1,
+    name: '已记账零售单',
+    color: 'bgColor',
+    view: { url: 'SalesSlip:list' },
+    import: '/thirdpartypay/salesslip/salesImport.do',
+    y: 80,
+    x: 50
+  }, {
+    id: 2,
+    img: 'rightArrow',
+    y: 80,
+    x: 150
+  }, {
+    id: 3,
+    name: '已合并零售单',
+    color: 'bgColor',
+    view: { url: 'SalesSlipCheckedData:list', param: { platform: '4' } },
+    import: '',
+    y: 80,
+    x: 250
+  }, {
+    id: 4,
+    img: 'cycle',
+    check: { param: '1' },
+    y: 80,
+    x: 350
+  }, {
+    id: 3,
+    name: '合并后钱包账户流水',
+    color: 'bgColor',
+    view: { url: 'PlatformSettlementDataJD:list', param: { platform: '4', tenantid: '1' } },
+    import: '',
+    y: 80,
+    x: 450
+  }, {
+    id: 2,
+    img: 'leftArrow',
+    y: 80,
+    x: 560
+  }, {
+    id: 4,
+    name: '钱包账户流水',
+    color: 'bgColor',
+    view: { url: 'AlipayBillFlow:list', param: { platform: '4', tenantid: '1' } },
+    import: 'thirdpartypay/alipaybillflow/flowImport.do',
+    y: 80,
+    x: 650
+  }]
+}
+export { flowChartJD, flowChartTM }
--- src/page/npmContent/index.tsx
+++ src/page/npmContent/index.tsx
@@ -1,159 +1,292 @@
+import 'jquery';
 import React from 'react';
-import Command from './command';
-import flowChartJD from './aaa'
-export default class ReactContent extends React.Component {
-    renderButton = (data: any) => {
-        switch (data.type) {
-            case 'view':
-                return <a>查看</a>
-            case 'import':
-                return <a>导入</a>
-            case 'check':
-                return <a>核对处理</a>
-        }
-    }
-    renderChildren = (data: any) => {
-        let content;
-        if (data.type === 'horizontal-block') {
-            let text = data.nodeList.map((a: any, i: any) => {
-                if (a.type === 'block') {
-                    return (
-                        <div style={{ display: 'inline-block' }}>
-                            <div style={{ background: 'pink' }}>
-                                {a.text}
-                            </div>
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                        </div>
-                    )
-                } else if (a.type === 'cycle') {
-                    return (
-                        <div style={{ display: 'inline-block' }}>
-                            <img src={require('./img/cycle.jpg')} width="56px" height="48px" />
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                        </div>
-                    )
-                }
-            })
-            content = (
-                <div style={{ border: '1px dotted #c9c7c7', display: 'inline-block' }}>
-                    {text}
-                </div>
-            )
+import 'create-react-class';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { RMessage, RPanel, RButton, RModal, RFormItem } from '@components';
+import { RImport } from '@BComponents';
+import { post } from '@common/utils/http/http';
+import './index.less';
+
+// 节点背景色
+const bgColorEnum = {
+  childColor: '#38C394',
+  horColor: '#13A7F9',
+  verColor: '#9184F1'
+};
+let leftWidth = 0
+let middleWidth = 0
+let rightWidth = 0
+export default class RFlowChart extends React.PureComponent {
+  static propTypes = {
+    /**
+     * 流程图title
+     */
+    title: PropTypes.string,
+    /**
+     * 传入父组件的属性，该值只要写"this.props"即可
+     */
+    propsObj: PropTypes.object,
+    /**
+     * 描述节点的列表
+     */
+    flowList: PropTypes.object,
+    /**
+     * 描述节点的请求地址
+     */
+    flowAPI: PropTypes.string,
+    /**
+     * 查询描述节点请求时携带的参数
+     */
+    flowParams: PropTypes.object,
+    /**
+     * 外部传进来的参数，核对处理的时候会用到
+     */
+    propsParams: PropTypes.object
+  };
+  static defaultProps = {
+    title: null,
+    propsObj: null,
+    flowList: {},
+    flowAPI: null,
+    flowParams: {},
+    propsParams: {}
+  }
+  state = {
+    flowArray: {} // 用来存储节点信息
+  }
+  componentDidMount() {
+    this.getFlowList()
+  }
+  // 获取描述节点信息的列表
+  getFlowList = () => {
+    const { flowAPI, flowParams, flowList } = this.props
+    if (Object.keys(flowList) > 0) {
+      this.setState({
+        flowArray: flowList
+      })
+    } else if (flowAPI) {
+      flowAPI({ ...flowParams }).then((res) => {
+        if (res.code === '0') {
+          this.setState({
+            flowArray: res.data
+          })
         } else {
-            let text = data.nodeList.map((a: any, i: any) => {
-                if (a.type === 'block') {
-                    return (
-                        <div>
-                            <div style={{ background: 'pink' }}>
-                                {a.text}
-                            </div>
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                        </div>
-                    )
-                } else if (a.type === 'cycle') {
-                    return (
-                        <div>
-                            <img src={require('./img/cycle.jpg')} width="56px" height="48px" />
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                        </div>
-                    )
-                }
-            })
-            let top = data.children.map((a: any) => {
-                if (a.type === 'block') {
-                    return (
-                        <div>
-                            <div style={{ background: 'pink' }}>
-                                {a.text}
-                            </div>
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                            <img src={require('./img/downArrow.jpg')} width="48px" height="76px" />
-                        </div>
-                    )
-                } else if (a.type === 'cycle') {
-                    return (
-                        <div>
-                            <img src={require('./img/cycle.jpg')} width="56px" height="48px" />
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                        </div>
-                    )
-                }
-            })
-            content = (
-                <div>
-                    {top}
-                    <div style={{ border: '1px dotted #c9c7c7', display: 'inline-block' }}>
-                        {text}
-                    </div>
-                </div>
-            )
+          this.setState({
+            flowArray: {}
+          })
         }
-        return content;
+      })
     }
-    renderLeftChildren = (data: any) => {
-        if (data.children) {
-            let text = data.children.map((a: any) => {
-                return (
-                    <div style={{ display: 'inline-block' }}>
-                        <div style={{ display: 'inline-block' }}>
-                            <div style={{ background: 'pink' }}>
-                                {a.text}
-                            </div>
-                            <p>
-                                {a.button.map((b: any) => this.renderButton(b))}
-                            </p>
-                        </div>
-                        <div style={{ display: 'inline-block' }}>
-                            <img src={require('./img/rightArrow.jpg')} />
-                        </div>
-                    </div>
-                )
-            })
-            return text;
-        }
+  }
+  componentDidUpdate(preProps) {
+    if (preProps.flowParams !== this.props.flowParams) {
+      this.getFlowList()
     }
-    renderRightChildren = (data: any) => {
-        let content;
-        if (data.children) {
-            let text = data.children.map((a: any) => {
-                if (a.children) {
-                    let rightCore = a.children.map((b: any) => this.renderChildren(b))
-                    return rightCore
-                }
-
-            })
-        }
-    }
-    renderTopChildren = (data: any) => {
-
-    }
-    renderAll = () => {
-        const flowData = flowChartJD.data
+  }
+  view = (data) => {
+    let { propsObj } = this.props;
+    const { forward } = propsObj;
+    forward(data.url, {
+      jumpParam: {
+        closable: true,
+        param: data.param
+      }
+    });
+  }
+  check = (data) => {
+    // post(data.url, this.props.propsParams).then((res) => {
+    //   if (res.returnCode === 0) {
+    //     RMessage.success('核对成功')
+    //   } else {
+    //     RMessage.error(res.errorInfo)
+    //   }
+    // })
+  }
+  renderButton = (data) => {
+    switch (data.type) {
+      case 'view':
+        return <a onClick={() => this.view(data)}>{data.text}</a>
+      case 'import':
         return (
-            <div>
-                {this.renderLeftChildren(flowData.nodeList[0])}
-                {this.renderChildren(flowData)}
-                {this.renderRightChildren(flowData.nodeList[2])}
-            </div>
-        )
+          <RImport
+            title={data.text}
+            btnStyle="a"
+            importUrl={data.url}
+          />)
+      case 'check':
+        return <a>{data.text}</a>
+      default:
+        return '';
     }
-    render() {
+  }
+  // 块的不同情况
+  renderBlock = (a, type) => {
+    console.log('打印，', type)
+    if (type === 'right') {
+      let width = document.documentElement.clientWidth - 200
+      let totalWidth = rightWidth + leftWidth + middleWidth
+      if (totalWidth > width - 270) {
+        console.log('widthLL：：', rightWidth + leftWidth + middleWidth , width - 250)
+        // eslint-disable-next-line no-param-reassign
+        type = 'top'
+      }
+    }
+    switch (type) {
+      case 'ver':
+      case 'hor':
         return (
-            <div>
-                {this.renderAll()}
+          <div className={type === 'hor' ? 'block-inline' : null}>
+            <div className={`block ${type === 'hor' ? 'block-hor' : 'block-ver'}`}>
+              {a.text}
             </div>
+            <p className="block-button">
+              {a.button ? a.button.map(b => this.renderButton(b)) : ''}
+            </p>
+          </div>
         )
+      case 'left':
+      case 'right':
+        return (
+          <div className="block-inline">
+            <div className={type === 'left' ? 'hidden' : 'block-inline img'}>
+              <div className="img-left">{a.linkText}</div>
+            </div>
+            <div className="block-inline">
+              <div className="block block-normal">
+                {a.text}
+              </div>
+              <p className="block-button">
+                {a.button ? a.button.map(b => this.renderButton(b)) : ''}
+              </p>
+            </div>
+            <div className={type === 'left' ? 'block-inline img' : 'hidden'}>
+              <div className="img-right">{a.linkText}</div>
+            </div>
+          </div>
+        )
+      case 'top':
+        return (
+          <div className="block-inline">
+            <div className="block block-normal">
+              {a.text}
+            </div>
+            <p className="block-button">
+              {a.button ? a.button.map(b => this.renderButton(b)) : ''}
+            </p>
+            <div className="block-inline">
+              <img src={require('./img/downArrow.png')} width="48px" height="76px" />
+            </div>
+          </div>
+        )
+      default:
+        return ''
     }
-} 
+  }
+  // 循环的那个图
+  renderCycle = (data, type) => (
+    <div className={type === 'hor' ? 'block-inline' : ''}>
+      <img src={require('./img/cycle.png')} alt="核对处理" width="60px" height="45px" />
+      <p className="block-button">
+        {data.button ? data.button.map(i => this.renderButton(i)) : ''}
+      </p>
+    </div>
+  )
+  // 通过判断type类型，来选择是返回普通块，还是有框框的块
+  renderCore = (data, type) => {
+    switch (data.type) {
+      case 'horizontal-cycle':
+        middleWidth += 388
+        return (
+          <div className="block-inline border-block border-block-hor">
+            <div className="block-hor-content">
+              {this.renderBlock(data.nodeList[0], 'hor')}
+              {this.renderCycle(data, 'hor')}
+              {this.renderBlock(data.nodeList[1], 'hor')}
+            </div>
+          </div>
+        )
+      case 'vertical-cycle':
+        if (type === 'right') {
+          rightWidth += 293
+        } else {
+          leftWidth += 293
+        }
+        return (
+          <div className="block-inline">
+            <div className={type === 'right' ? 'block-inline img img-ver' : 'hidden'}>
+              <div className="img-left">{data.linkText}</div>
+            </div>
+            <div className="block-inline border-block border-block-ver">
+              {this.renderBlock(data.nodeList[0], 'ver')}
+              {this.renderCycle(data, 'ver')}
+              {this.renderBlock(data.nodeList[1], 'ver')}
+            </div>
+            <div className={type === 'left' ? 'block-inline img img-ver' : 'hidden'}>
+              <div className="img-right">{data.linkText}</div>
+            </div>
+          </div >
+        )
+      case 'block':
+        if (type === 'right') {
+          rightWidth += 236
+        } else {
+          leftWidth += 236
+        }
+        return this.renderBlock(data, type)
+      default:
+        return ''
+    }
+  }
+  // 两边的内容
+  renderChildren = (data, array, type) => {
+    let text = data.map((a) => {
+      if (a.children) {
+        return this.renderChildren(a.children, [this.renderCore(a, type), array], type)
+      }
+      return [this.renderCore(a, type), array]
+    })
+    return type === 'left' ? text.reverse() : text;
+  }
+  renderAll = () => {
+    const { flowArray } = this.state;
+    if (Object.keys(flowArray).length) {
+      return (
+        <div>
+          <div className="block-inline left-content">
+            {flowArray.nodeList[0].children ? this.renderChildren(flowArray.nodeList[0].children, null, 'left') : ''}
+          </div>
+          <div className="block-inline middle-content">
+            {this.renderCore(flowArray)}
+          </div>
+          <div className="block-inline right-content">
+            {flowArray.nodeList[1].children ? this.renderChildren(flowArray.nodeList[1].children, null, 'right') : ''}
+          </div>
+        </div>
+      )
+    }
+    return ''
+  }
+  render() {
+    const { title } = this.props;
+    if (document.getElementsByClassName('flow-chart-wrap').length) {
+      let height = document.getElementsByClassName('flow-chart-wrap')[0].clientHeight
+      document.getElementsByClassName('flow-chart-content')[0].style.height = `${height}px`
+    }
+    if (document.getElementsByClassName('left-content').length) {
+      let width = document.getElementsByClassName('left-content')[0].clientWidth
+      console.log('打印宽：', width)
+    }
+    return (
+      <div className="flow-chart-wrap">
+        <h3 className="flow-chart-title">
+          <span style={{ marginRight: '20px' }}>稽核场景：{title.scene}</span>
+          <span>稽核产品：{title.product}</span>
+        </h3>
+        <div className="flow-chart-content">
+          {this.renderAll()}
+        </div>
+      </div>
+    );
+  }
+}
